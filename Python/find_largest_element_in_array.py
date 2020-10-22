c5d462e019aaddd47910debaf6c6d605d6afab62
DamianeK02 <70578851+DamianeK02@users.noreply.github.com>
2020-10-22 21:47:48 +0200

    Create find_largest_element_in_array.py
    i added my own python program

---
+++ Python/find_largest_element_in_array.py
@@ -0,0 +1,20 @@
+
+#turn on with "python python_find_largest_element_in_array_fastest_method.py"
+
+# you can enter an unlimited number of numbers here :D
+array = [10, 20, 90, 70, 8, 4] 
+
+def lengths(array,n): 
+
+    max = array[0] 
+
+    for i in range(1, number): 
+        if array[i] > max: 
+            max = array[i] 
+    return max
+
+number = len(array) 
+
+biggest_number = lengths(array,number) 
+
+print(biggest_number)
