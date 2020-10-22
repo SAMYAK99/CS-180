7d5864da665a61037dd65d5b502bf92ee2834cc9
DamianeK02 <70578851+DamianeK02@users.noreply.github.com>
2020-10-22 21:59:32 +0200

    added  python argv system example
    you can use that to make amazing things :D

---
+++ Python/PrintFromArgv.py
@@ -0,0 +1,8 @@
+#python .\PrintFromArgv.py argument
+
+import sys
+
+print(sys.argv[1])
+
+#it gives you many opportunities of use
+#You can add sys.argv[1] + sys.argv[2] or add it to table, its amazing
