4ede2c1a7f89f247f3dfcde54afad2ba828e2d0b
Richa Mishra <richamishra2018@gmail.com>
2020-10-28 00:02:22 +0530

    Added Union_of_2_array

--- C/infixToPostfix.c
+++ C/infixToPostfix.c
@@ -1,125 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <string.h>
+#include<string.h>
+#include <limits.h>
+#define MAX 10000
 
-char in_str[20], post_str[20];
-int top;
-int stack[20];
-int pop();
-int precedence(char symbol);
-int isEmpty();
-void InfixToPostfix();
-int check_space(char symbol);
-void push(long int symbol);
+int stack[MAX], top = -1;
+
+void push(int item) 
+{ 
+    if (top == MAX - 1){
+        printf("Max Size reached\n");
+        return; 
+    }
+
+    stack[++top] = item; 
+}
+
+int pop() 
+{ 
+    if (top==-1) 
+        return INT_MIN; 
+    return stack[top--]; 
+}
+
+int checkIfOperand(char ch) 
+{ 
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'); 
+} 
+
+int precedence(char ch) 
+{ 
+    switch (ch) 
+    { 
+    case '+': 
+    case '-': 
+        return 1; 
+
+    case '*': 
+    case '/': 
+        return 2; 
+
+    case '^': 
+        return 3; 
+    } 
+    return -1; 
+} 
+
+int getPostfix(char *expression) 
+{ 
+    int i, j;
+
+    for (i = 0, j = -1; expression[i]; ++i) 
+    { 
+        if (checkIfOperand(expression[i])) 
+            expression[++j] = expression[i]; 
+
+        else if (expression[i] == '(') 
+            push(expression[i]); 
+
+        else if (expression[i] == ')') 
+        { 
+            while (top!=-1 && stack[top] != '(') 
+                expression[++j] = pop(stack); 
+            if (top!=-1 && stack[top] != '(') 
+                return -1;              
+            else
+                pop(); 
+        } 
+        else
+        { 
+            while (top!=-1 && precedence(expression[i]) <= precedence(stack[top]))
+                expression[++j] = pop(); 
+            push(expression[i]); 
+        } 
+
+    } 
+    while (top!=-1) 
+        expression[++j] = pop(); 
+
+    expression[++j] = '\0'; 
+    
+}
 
 int main()
-{
-int count, length;
-char temp;
-top = -1;
-printf("\nEnter a Infix Expression : ");
-scanf("%s", in_str);
-InfixToPostfix();
-printf("\nThe Equivalent postfix expression will be : %s\n", post_str);
-return 0;
+{    
+    printf("The infix is: ");
+
+    char expression[] = "((a/b)+c)-(d+(e*f))"; 
+    printf("%s\n",expression);
+    getPostfix(expression); 
+
+    printf("The postfix is: ");
+    printf("%s\n",expression);
+
+    return 0; 
 }
 
-void InfixToPostfix()
-{
-unsigned int count, temp = 0;
-char next;
-char symbol;
-for(count = 0; count < strlen(in_str); count++)
-{
-    symbol = in_str[count]; 
-    if(!check_space(symbol))
-{
- switch(symbol)
-  {
-    case '(': push(symbol);
-    break;
-    case ')': while((next = pop()) != '(') 
-                {
-                post_str[temp++] = next;
-                }
-                break;
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-    case '%':
-    case '^': while(!isEmpty() && precedence(stack[top]) >= precedence(symbol)) 
-                post_str[temp++] = pop();
-                push(symbol);
-                break;
-    default:post_str[temp++] = symbol;
-  }
- }
-}
-while(!isEmpty())
- {
-  post_str[temp++] = pop();
- }
-  post_str[temp] = '\0';
-}
-
-int precedence(char symbol)
-{
- switch(symbol)
-  {
-   case '(': return 0;
-   case '+':
-   case '-':return 1;
-   case '*':
-   case '/':
-   case '%':return 2;
-   case '^': return 3;
-   default: return 0;
-  }
-}
-
-int check_space(char symbol)
-{
-if(symbol == '\t' || symbol == ' ' )
-   {
-    return 1;
-   }
-else
-   {
-    return 0;
-   }
-}
-
-void push(long int symbol)
-{
-if(top > 20)
-    {
-    printf("Stack Overflow\n");
-    exit(1);
-    }
-top = top + 1;
-stack[top] = symbol; 
-}
-
-int isEmpty()
-{
-if(top == -1)
-    {
-    return 1;
-    }
-else
-    {
-    return 0;
-    }
-}
-
-int pop()
-{
-if(isEmpty())
-   { 
-    printf("Stack is Empty\n");
-    exit(1);
-   }
-return(stack[top--]); 
-}
+/*
+OUTPUT:
+The infix is: ((a/b)+c)-(d+(e*f))
+The postfix is: ab/c+def*+-
+*/
