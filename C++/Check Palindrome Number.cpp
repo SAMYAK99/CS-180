963c52c4a7a58addae9e4dd6b1b8bc5f9c42e5c5
Neharika Srivastav <43313920+neharikasrivastav@users.noreply.github.com>
2020-10-31 03:42:13 +0530

    updated check palindromic no.

--- C++/Check Palindrome Number.cpp
+++ C++/Check Palindrome Number.cpp
@@ -2,23 +2,30 @@
 using namespace std;
 
 int main(){
-	int numberDigits;
+	int nd;
 	cout << "How Many Digits Number Would you want to check : ";
-	cin >> numberDigits; cout << endl;
-	char number[numberDigits], number2[numberDigits];
+	cin >> nd; 
+	cout << endl;
+	char number[nd], number2[nd];
 	cout << "Input your number : ";
-	for(int i=1; i<=numberDigits; i++){
+	for(int i=1; i<=nd; i++){
 		cin >> number[i];
 	} cout << endl;
 	
-	// change value inverse
-	for(int i=1; i<=numberDigits; i++){
-		number2[i] = number[numberDigits+1-i];
+	for(int i=1; i<=nd; i++)
+	{
+		number2[i] = number[nd+1-i];
 	}
 
 	// Check number value
-	for(int i=1; i<=numberDigits; i++){
-		if(number[i] != number2[i]) { cout << "This number is not palindrome"; return 0;}
+	for(int i=1; i<=nd; i++)
+	{
+		if(number[i] != number2[i])
+		{
+			cout << "This number is not palindrome";
+			return 0;
+		}
 	}
-	cout << "This number palindrome"; return 1; 
+	cout << "This number palindrome"; 
+	return 1; 
 }
