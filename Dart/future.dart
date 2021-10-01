f08edbdaf3e41f7eef31cde9dc04dea7497fed42
Ashish Kumar <70542481+ashishworkspace@users.noreply.github.com>
2021-10-01 12:51:51 +0530

    Future in dart

---
+++ Dart/future.dart
@@ -0,0 +1,14 @@
+// Future in dart
+
+Future<void> main() async {
+  print(await showUserData());
+}
+
+Future<String> fetchUserData() async {
+  return await Future.delayed(Duration(seconds: 3), () => "Nice");
+}
+
+Future<String> showUserData() async {
+  String data = await fetchUserData();
+  return "This is user data: $data";
+}
