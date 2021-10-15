7593b69736e604a96db7d0a752d4f4f2b0210ee7
Jeet Patel <60734679+rebeljeet@users.noreply.github.com>
2021-10-15 19:47:42 +0530

    Update Merge sort.cpp

--- C++/Merge sort.cpp
+++ C++/Merge sort.cpp
@@ -24,28 +24,31 @@ void merge(int *arr, int low, int high, int mid)
    c[k] = arr[i];
    k++;
    i++;
-}
-else {
-   c[k] = arr[j];
-   k++;
-   j++;
    }
-}
+   else {
+    c[k] = arr[j];
+    k++;
+    j++;
+    }
+   }
   
 while (i <= mid) {
    c[k] = arr[i];
    k++;
    i++;
 }
+    
 while (j <= high) {
    c[k] = arr[j];
    k++;
    j++;
 }
+    
 for (i = low; i < k; i++) {
    arr[i] = c[i];
 }
 }
+
 int main()
 {
     int a[12]={21,11,10,30,4,7,9,20,18,1,15,8};
