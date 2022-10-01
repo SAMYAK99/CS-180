562e703f08eaeceec8673bf4d55e78abbaa61788
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-10-01 22:26:29 +0530

    edited  tic-tac-toe.py

--- Python/tic-tac-toe.py
+++ Python/tic-tac-toe.py
@@ -41,3 +41,14 @@ class TicTacToeGame:
             for row in range(self.board_size)
         ]
         self._winning_combos = self._get_winning_combos()
+    
+    def _get_winning_combos(self):
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+    
