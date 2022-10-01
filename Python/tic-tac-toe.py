b3f7128684130f46f563c40a9d28639d2ca677ea
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-10-01 22:22:59 +0530

    added tic-tac-toe.py

---
+++ Python/tic-tac-toe.py
@@ -0,0 +1,43 @@
+"""A tic-tac-toe game built with Python and Tkinter."""
+
+import tkinter as tk
+from itertools import cycle
+from tkinter import font
+from typing import NamedTuple
+
+
+class Player(NamedTuple):
+    label: str
+    color: str
+
+
+class Move(NamedTuple):
+    row: int
+    col: int
+    label: str = ""
+
+
+BOARD_SIZE = 3
+DEFAULT_PLAYERS = (
+    Player(label="X", color="blue"),
+    Player(label="O", color="green"),
+)
+
+
+class TicTacToeGame:
+    def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+        self._players = cycle(players)
+        self.board_size = board_size
+        self.current_player = next(self._players)
+        self.winner_combo = []
+        self._current_moves = []
+        self._has_winner = False
+        self._winning_combos = []
+        self._setup_board()
+
+    def _setup_board(self):
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range(self.board_size)
+        ]
+        self._winning_combos = self._get_winning_combos()
