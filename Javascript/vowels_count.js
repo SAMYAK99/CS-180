a8590c3d14eb9bdaeb1812ed2850c1ad9e374bd0
Abhishek Sharma <abhisharma85955@gmail.com>
2022-10-05 17:36:24 +0530

    Added a program to count no. of vowels in string using for loop

---
+++ Javascript/vowels_count.js
@@ -0,0 +1,26 @@
+// program to count the number of vowels in a string
+
+// defining vowels
+const vowels = ["a", "e", "i", "o", "u"]
+
+function countVowel(str) {
+    // initialize count
+    let count = 0;
+
+    // loop through string to test if each character is a vowel
+    for (let letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            count++;
+        }
+    }
+
+    // return number of vowels
+    return count
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+const result = countVowel(string);
+
+console.log(result);
