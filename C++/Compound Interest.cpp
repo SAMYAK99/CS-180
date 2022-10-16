b132dd9b0fd60722b70980f45c9de93340426c43
amin <101981614+Meta-zamin@users.noreply.github.com>
2022-10-16 16:00:05 +0530

    Added variables in the code.

--- C++/Compound Interest.cpp
+++ C++/Compound Interest.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
-	double principle = 10000, rate = 5, time = 2;
-
+	
+	double principle, rate;
+	int time;
+	cout<<"Input the values of principle, rate and time";
+	cin>>principle;
+	cin>>rate;
+	cin>>time;
 	/* Calculate compound interest */
 	double A = principle * (pow((1 + rate / 100), time));
 	double CI = A- principle;
-
 	cout << "Compound interest is " << CI;
-
 	return 0;
 }
