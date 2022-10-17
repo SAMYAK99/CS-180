b0b2b5d7b472f513c601b193054335e14495f3d7
Rohit Patil <71687498+falconcode16@users.noreply.github.com>
2022-10-17 18:31:54 +0530

    Create Surface_and_volume_of_Sphere.c

---
+++ C/Surface_and_volume_of_Sphere.c
@@ -0,0 +1,16 @@
+
+
+#include<stdio.h>
+#include<math.h>
+
+main()
+{
+    float vol,surf,r;
+
+     r=8;
+   vol=(4*3.14*r*r*r)/3;
+   surf=4*3.14*r*r;
+    printf("vol=%f\n",vol);
+    printf("surf=%f\n",surf);
+
+}
