690bbf520490a637b25a5583cfbb34ee80f526d8
Nikita-choudhary09 <90494881+Nikita-choudhary09@users.noreply.github.com>
2022-10-19 21:41:37 +0530

    Create numberof1bits.cpp

---
+++ numberof1bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n!=0){
+            if (n&1){
+                count++;
+            }
+            n = n>>1;
+        }
+       return count; 
+    }
+};
