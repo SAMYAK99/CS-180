8ccd6d7a8b7dfdf3e16021ffd2e46e83905a08cc
ShauryaDev9 <shaurya.21scse1011014@galgotiasuniversity.edu.in>
2022-10-19 19:26:27 +0530

    swapping numbers
    swap numbers

--- C++/Inorder_Traversal.cpp
+++ C++/Inorder_Traversal.cpp
@@ -1,53 +1,39 @@
-// C++ program for inorder tree traversals
 #include <bits/stdc++.h>
 using namespace std;
 
-/* A binary tree node has data, pointer to left child
-and a pointer to right child */
 struct Node {
-	int data;
-	struct Node *left, *right;
+    int data;
+    struct Node *left, *right;
 };
 
-// Utility function to create a new tree node
 Node* newNode(int data)
 {
-	Node* temp = new Node;
-	temp->data = data;
-	temp->left = temp->right = NULL;
-	return temp;
+    Node* temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
 }
-
-/* Given a binary tree, print its nodes in inorder*/
+ 
 void printInorder(struct Node* node)
 {
-	if (node == NULL)
-		return;
-
-	/* first recur on left child */
-	printInorder(node->left);
-
-	/* then print the data of node */
-	cout << node->data << " ";
-
-	/* now recur on right child */
-	printInorder(node->right);
+    if (node == NULL)
+        return;
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
 }
-
-/* Driver code*/
+ 
 int main()
 {
-	struct Node* root = newNode(1);
-	root->left = newNode(2);
-	root->right = newNode(3);
-	root->left->left = newNode(4);
-	root->left->right = newNode(5);
-
-
-	// Function call
-	cout << "\nInorder traversal of binary tree is \n";
-	printInorder(root);
-
-
-	return 0;
+    struct Node* root = newNode(5);
+    root->left = newNode(9);
+    root->right = newNode(0);
+    root->left->left = newNode(3);
+    root->left->right = newNode(7);
+ 
+    cout << "\nInorder traversal of binary tree is \n";
+    printInorder(root);
+ 
+ 
+    return 0;
 }
